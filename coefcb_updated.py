34b467cab7ad5dc733ce574a9b966e86f942e13d
Yuri Silva <121751241+yuribertozzi@users.noreply.github.com>
2023-01-08 16:35:27 -0300

    Add files via upload

---
+++ coefcb_updated.py
@@ -0,0 +1,42 @@
+def fatorial (x):
+
+	if x == 0:
+	
+		return(1)
+
+	else:
+
+		f = 1
+
+		while x > 1:
+
+			f = f * x
+
+			x = x - 1
+
+		return(f)
+
+
+# m!/((m-n)!n!)
+
+
+print()
+
+m = int(input("Entre com o valor de m: "))
+
+n = int(input("Entre com o valor de n: "))
+
+
+coeficiente = fatorial(m)/(fatorial(m - n)*fatorial(n))
+
+
+print()
+
+
+if m < n or m < 0 or n < 0:
+
+	print("Não existe fatorial de número negativo.")
+
+else:
+
+	print("O coeficiente binomial é", coeficiente, ".")
